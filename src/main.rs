dbf1ac7e5206bfef06e4c055773829c1d9fed938
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2024-10-19 19:33:48 +0530

    A basic kernel

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,30 @@
 // disable all Rust-level entry points
 #![no_main]
 
-// overwriting the entrypoint
-// no_mangle to disable cryptic function naming
-#[no_mangle]
-// _start is default entrypoint for most systems
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 use core::panic::PanicInfo;
 
 /// This function is called on panic. originally found in std but we are using no_std env
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+// overwriting the entrypoint
+// no_mangle to disable cryptic function naming
+#[no_mangle]
+// _start is default entrypoint for most systems
+pub extern "C" fn _start() -> ! {
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
